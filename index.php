12b18f0ceb1894db6f521fd0bc41051cfb655f76
EnricoClem <enrico.clem94@gmail.com>
2024-05-07 15:21:56 +0200

    SETTING - html form with bootstrap

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+    <main>
+        <div class="contianer-sm p-4">
+            <div>
+                <h1>Iscriviti alla nostra newsletter</h1>
+            </div>
+            <div>
+                <form action="">
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Inserisci la tua mail...">
+                    </div>
+                    <button type="button" class="btn btn-primary">Iscriviti</button>
+                </form>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
